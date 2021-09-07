92d64c6f260a2f6b61d1c49097e9a3405740c2af
Gui Feitosa <guifeitos@gmail.com>
2021-09-07 15:18:46 -0300

    Criação de uma nova ViewController: SegundaViewController
     - Criação de uma Label e utilizado auto layout
     - Criação de uma CollectionView e utilizado auto layout

---
+++ AppAnimais/AppAnimais/SegundaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SegundaViewController.swift
+//  AppAnimais
+//
+//  Created by Gui Feitosa on 07/09/21.
+//
+
+import UIKit
+
+class SegundaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
