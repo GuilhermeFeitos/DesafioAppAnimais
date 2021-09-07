62c29417ddfc432a40c7ebd16b1bc4db33490ffa
Gui Feitosa <guifeitos@gmail.com>
2021-09-07 14:22:48 -0300

    Criação Projeto
    Habilitado Navigation Controller

---
+++ AppAnimais/AppAnimais/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppAnimais
+//
+//  Created by Gui Feitosa on 07/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
